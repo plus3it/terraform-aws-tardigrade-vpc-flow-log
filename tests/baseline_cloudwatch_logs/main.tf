debd0b4beaed2d994c78a489210d808d30c8b0a5
confusdcodr <confusdcodr@users.noreply.github.com>
2019-09-10 15:38:28 -0400

    Upgrade terraform to 0.12.x

---
+++ tests/baseline_cloudwatch_logs/main.tf
@@ -0,0 +1,25 @@
+provider aws {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "github.com/terraform-aws-modules/terraform-aws-vpc?ref=v2.15.0"
+
+  providers = {
+    aws = aws
+  }
+
+  name = "tardigrade-vpc-flow-log-testing"
+  cidr = "10.0.0.0/16"
+}
+
+module "baseline_s3" {
+  source = "../../"
+  providers = {
+    aws = aws
+  }
+
+  create_vpc_flow_log = true
+  vpc_id = module.vpc.vpc_id
+  log_destination_type = "cloud-watch-logs"
+}
--- variables.tf
+++ variables.tf
@@ -4,37 +4,38 @@ variable "create_vpc_flow_log" {
 }
 
 variable "log_destination_type" {
-  description = "Controls whether to create the VPC Flow Log with a CloudWatch Logs or S3 bucket destination"
-  type        = "string"
-  default     = ""
+  description = "Controls whether to create the VPC Flow Log with a `cloud-watch-logs` or `s3` bucket destination"
+  type        = string
+  default     = null
 }
 
 variable "vpc_id" {
   description = "VPC ID for which the VPC Flow Log will be created"
-  type        = "string"
-  default     = ""
+  type        = string
+  default     = null
 }
 
 variable "iam_role_arn" {
   description = "(Optional) ARN for the IAM role to attach to the flow log. If blank, a minimal role will be created"
-  type        = "string"
-  default     = ""
+  type        = string
+  default     = null
 }
 
 variable "log_destination" {
   description = "(Optional) The ARN of the logging destination."
-  type        = "string"
-  default     = ""
+  type        = string
+  default     = null
 }
 
 variable "log_group_name" {
-  description = "(Optional) Name to assign to the CloudWatch Log Group. If blank, will use `/aws/vpc/flow-log/$${var.vpc_id}`"
-  type        = "string"
-  default     = ""
+  description = "(Optional) Name to assign to the CloudWatch Log Group. If blank, will use `/aws/vpc/flow-log/$$${var.vpc_id}`"
+  type        = string
+  default     = null
 }
 
 variable "tags" {
   description = "A map of tags to add to the CloudWatch Log Group for the VPC Flow Log"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
+
